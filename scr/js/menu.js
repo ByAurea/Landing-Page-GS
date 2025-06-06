3ad9990de010513660d3a8733e0898d73e8e637f
byaurea <aureacarminato@gmail.com>
2025-06-05 23:24:51 -0300

    fix/menu hamburguer arrumado

--- scr/js/menu.js
+++ scr/js/menu.js
@@ -1,6 +1,16 @@
-const toggleButton = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const nav = document.querySelector('nav'); // Seleciona o elemento <nav>
+    const navLinks = document.querySelectorAll('.nav-list li a'); // Seleciona todos os links do menu
 
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+    mobileMenu.addEventListener('click', () => {
+        nav.classList.toggle('active'); // Alterna a classe 'active' no elemento <nav>
+    });
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('active')) {
+                nav.classList.remove('active');
+            }
+        });
+    });
 });
